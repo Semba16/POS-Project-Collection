8e99234895fd65ff4844e99a0a0c1df67a94a733
Semba Wijaya <2131710085@student.polinema.ac.id>
2024-02-29 18:57:29 +0700

    progres

--- PWL_2024/routes/web.php
+++ PWL_2024/routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/hello', function () {
+    return 'SELAMAT DATANG';
+   });
+   Route::get('/about', function () {
+    return '2141762085';
+   });
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+    
