72920e4664374fbbe6eedf318fe1898b7a44cf0f
Semba Wijaya <2131710085@student.polinema.ac.id>
2024-02-29 21:29:09 +0700

    Resource Controller

--- PWL_2024/routes/web.php
+++ PWL_2024/routes/web.php
@@ -1,9 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Route::get('/hello', function () {
-    return 'SELAMAT DATANG';
-   });
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+   
+   
+   Route::get('/world', function () {
+    return 'World';
+   });   
+   
    Route::get('/about', function () {
     return '2141762085';
    });
@@ -16,3 +34,21 @@ Route::get('/hello', function () {
      return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
     });
     
+    Route::get('/user/{name?}', function ($name=null) {
+        return 'Nama saya '.$name;
+    }); 
+
+    Route::get('/user/{name?}', function ($name='John') {
+        return 'Nama saya '.$name;
+    });
+
+    
+    Route::resource('photos', PhotoController::class);
+    
+
+    Route::resource('photos', PhotoController::class)->only([
+        'index', 'show'
+       ]);
+       Route::resource('photos', PhotoController::class)->except([
+        'create', 'store', 'update', 'destroy'
+       ]);
