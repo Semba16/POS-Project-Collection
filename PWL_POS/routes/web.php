95545fc2f0238924f7ba7549069158317e74abdc
Semba Wijaya <2131710085@student.polinema.ac.id>
2024-03-22 08:51:04 +0700

    Routing

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StokController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\PenjualanDetailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +23,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::get('/barang', [BarangController::class, 'index']);
+Route::get('/penjualan', [PenjualanController::class, 'index']);
+Route::get('/stok', [StokController::class, 'index']);
+Route::get('/penjualan-detail', [PenjualanDetailController::class, 'index']);
